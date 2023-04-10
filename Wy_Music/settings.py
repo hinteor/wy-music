f7a9eded11ce53ae28dc230d146aeb7f003fdc66
hinteor <45594723+hinteor@users.noreply.github.com>
2023-04-10 09:11:21 +0000

    return all web_ui python code

--- Wy_Music/settings.py
+++ Wy_Music/settings.py
@@ -38,8 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     
-    #"apps.web_ui",
-    #"apps.youtube_comunication",
+    "apps.web_ui",
     "apps.discord_comunication"
     
 ]
--- apps/youtube_comunication/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class YoutubeComunicationConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "apps.youtube_comunication"
